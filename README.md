049395ed1bcc0811fec571e981507cf80f7133d3
treastrain / Tanaka Ryoga <tr@tret.jp>
2025-08-20 23:07:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# XCIntelligenceBridge
+
---
+++ Sources/XCIntelligenceBridge/XCIntelligenceBridge.swift
@@ -0,0 +1,13 @@
+//
+//  XCIntelligenceBridge.swift
+//  XCIntelligenceBridge
+//
+//  Created by treastrain on 2025/08/20.
+//
+
+@main
+struct XCIntelligenceBridge {
+    static func main() {
+        print("Hello, world!")
+    }
+}
